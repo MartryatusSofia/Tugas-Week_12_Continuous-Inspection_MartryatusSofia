031c16b4dda3a56567b6b7166600f111b68e7f4a
MartryatusSofia <martryatuss@gmail.com>
2026-06-04 10:32:56 +0700

    Perubahan code 6

--- src/main/java/Counter.java
+++ src/main/java/Counter.java
@@ -46,28 +46,5 @@ public class Counter {
 	public int getCount() {
 		return count;
 	}
-
-	// simulasi code smell untuk SonarCloud
-	public void simulateBadCode() {
-
-		int unusedNumber = 100;
-		String unusedText = "contoh issue sonar";
-
-		if(count > 0) {
-			System.out.println("Count positif");
-		}
-
-		if(count > 0) {
-			System.out.println("Count positif");
-		}
-
-		if(count > 0) {
-			System.out.println("Count positif");
-		}
-
-		if(count > 0) {
-			System.out.println("Count positif");
-		}
-	}
 	
 }
